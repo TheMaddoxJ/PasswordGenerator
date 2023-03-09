2f166955aa1ac1544a198cd8a4f0d74a86d8f0fd
TheMaddoxJ <maddoxjohnson7@gmail.com>
2023-03-09 11:58:27 -0500

    commit -m "console log "you hit the button" changes"

--- Develop/script.js
+++ Develop/script.js
@@ -1,6 +1,10 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+function generatePassword () {
+  console.log("Hello! You hit the button!");
+}
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
