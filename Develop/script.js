36b1c3c201ca13ca3fc13f245ebc36a5482bb04b
TheMaddoxJ <maddoxjohnson7@gmail.com>
2023-03-13 14:00:36 -0400

    final changes - added parseInt to allow code to function correct.

--- Develop/script.js
+++ Develop/script.js
@@ -5,7 +5,10 @@ var generateBtn = document.querySelector("#generate");
 function generatePassword() {
   console.log("Hello! You hit the button!");
 
-  var length = prompt("How long would you like your password to be? Choose a pass between 8-128 characters.");
+  //Creating variable "characters" and leaving the value as empty.
+var characters = [];
+
+  var length = parseInt( prompt("How long would you like your password to be? Choose a pass between 8-128 characters."));
   console.log(length);
 
   //If length of pass is less than 8 or more than 128 then alert the user that it should be 8-128 characters.
@@ -14,11 +17,12 @@ function generatePassword() {
     generatePassword()
   }
 
+  //prompt for numbers inclusion or not.
   var numbers = confirm("Would you like your password to include numbers?");
   console.log(numbers);
 
-  let characters = '';
 
+//All the 'if' conditional statements for password content details.
   if (numbers === "Yes") {
     console.log("Hello" + numbers);
     characters += '0123456789'
@@ -45,13 +49,18 @@ function generatePassword() {
     characters += "!#$%&')(*-./:;<=>?@[]\^_`{|}~"
   }
 
+
+  //Setting an empty value for the passwordArray variable.
   let passwordArray = [];
 
-  //  while (passwordArray.length < length) {
-  //    const characters = characters[Math.floor(Math.random() * characters.length)];
-  //  }
+  //randomizing the password output with math.random and making sure the password length does not extend past the desired length chosen.
+   while (passwordArray.length < length) {
+     const singlecharacter = characters[Math.floor(Math.random() * characters.length)];
+     passwordArray.push(singlecharacter);
+   }
+   console.log(passwordArray);
 
-  passwordArray.push(characters)
+
   console.log(characters)
   return passwordArray.join('');
 }
